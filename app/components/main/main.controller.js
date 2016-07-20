8ed66f7027f5c8ec9dfc662a9caafa5cda8ab2bf
ellwood <ellwyna@gmail.com>
2016-07-20 23:20:39 +0300

    main functionality - completed

--- app/components/main/main.controller.js
+++ app/components/main/main.controller.js
@@ -1,11 +1,8 @@
 export default class MainController {
-    constructor($scope, $state, $http, $window, $location, $anchorScroll, $timeout) {
+    constructor($state, $http, $window, $timeout) {
         this.http = $http;
         this.state = $state;
-        this.$scope = $scope;
         this.$window = $window;
-        this.$location = $location;
-        this.$anchorScroll = $anchorScroll;
         this.$timeout = $timeout;
 
         if(!this.propList){
@@ -22,23 +19,32 @@ export default class MainController {
         };
     }
 
-    resetSearch(value) {
+    resetSearch(value, loc) {
         this.filter = {
             page: 0,
             place_name: value || '',
-            centre_point: ''
+            centre_point: loc || ''
         };
+        this.errorMessage = '';
+        this.errorLoadMore = '';
         this.loading = false;
     }
 
     searchLoc(page, value) {
-        if(value && !page) {
-            this.resetSearch(value);
-            this.propList = {};
+        if (value) {
+            this.filter.centre_point = '';
         }
-        if (this.filter.place_name) {
-            if(page){
-                this.filter.page ++;
+        if ((value && !page) || this.filter.centre_point) {
+            this.resetSearch(value, this.filter.centre_point);
+            this.propList = {};
+            this.locList = {};
+        }
+
+        this.errorLoadMore = '';
+
+        if (this.filter.place_name || this.filter.centre_point) {
+            if (page) {
+                this.filter.page++;
             } else {
                 this.filter.page = '1'
             }
@@ -46,13 +52,22 @@ export default class MainController {
 
         this.url = 'http://api.nestoria.co.uk/api?country=uk&pretty=1&encoding=json&listing_type=buy&action=search_listings&callback=JSON_CALLBACK';
 
-        if(this.filter.place_name){
-            this.loading = true;
-            angular.forEach(this.filter, (val, key) => {
-                if (val) {
-                    this.url += '&' + key + '=' + val;
-                }
-            });
+        if (this.filter.place_name || this.filter.centre_point) {
+            if (this.filter.place_name && !this.filter.centre_point) {
+                this.loading = true;
+                angular.forEach(this.filter, (val, key) => {
+                    if (val) {
+                        this.url += '&' + key + '=' + val;
+                    }
+                });
+            } else if (this.filter.centre_point) {
+                this.filter.place_name = '';
+                angular.forEach(this.filter, (val, key) => {
+                    if (val) {
+                        this.url += '&' + key + '=' + val;
+                    }
+                });
+            }
 
             this.http({
                 method: 'JSONP',
@@ -62,16 +77,36 @@ export default class MainController {
                     json_callback: 'JSON_CALLBACK'
                 }
             }).then((resp) => {
-                this.loading = false;
-                if(this.propList.length > 0) {
-                    this.propList = this.propList.concat(resp.data.response.listings);
-                } else {
-                    this.propList = resp.data.response.listings;
+                if (resp.data.response.listings.length == 0) {
+                    this.errorMessage = 'There were no properties found for the given location.';
+                }
+                if (resp.data.response.application_response_text == 'unknown location') {
+                    this.errorMessage = 'The location given was not recognised.';
+                }
+                if (resp.data.response.application_response_code < 200){
+                    if (this.propList.length > 0) {
+                        this.propList = this.propList.concat(resp.data.response.listings);
+                    } else {
+                        this.propList = resp.data.response.listings;
+                    }
+                } else if (resp.data.response.application_response_code >= 200) {
+                    this.locList = resp.data.response.locations;
                 }
                 this.totalResults = resp.data.response.total_results;
                 this.loadMore = this.propList.length < resp.data.response.total_results;
                 this.saveRecentSearch();
-            });
+            })
+                .catch(()=>{
+                    if (this.propList.length == 0) {
+                        this.errorMessage = 'An error occurred while searching. Please check your network connection and try again.';
+                    } else {
+                        this.errorLoadMore = 'An error occurred while searching. Please check your network connection and try again.';
+                        this.filter.page--;
+                    }
+                })
+                .finally(()=>{
+                    this.loading = false;
+                });
         }
     }
 
@@ -86,21 +121,24 @@ export default class MainController {
     }
 
     saveRecentSearch() {
-        var resentList = localStorage.getItem('recentSearch') && JSON.parse(localStorage.getItem('recentSearch')) || [];
-        var searchData = {
-            filter: this.filter,
-            data: this.propList,
-            loadMore: this.loadMore,
-            totalResults: this.totalResults
-        };
+        if (this.propList.length > 0 || this.locList > 0) {
+            var resentList = localStorage.getItem('recentSearch') && JSON.parse(localStorage.getItem('recentSearch')) || [],
+            searchData = {
+                filter: this.filter,
+                data: this.propList,
+                locations: this.locList,
+                loadMore: this.loadMore,
+                totalResults: this.totalResults
+            };
 
-        if(!resentList){
-            resentList.push(searchData);
-        } else {
-            resentList = resentList.filter(item => item.filter.place_name.toLowerCase() != searchData.filter.place_name.toLowerCase());
-            resentList.unshift(searchData);
+            if(!resentList){
+                resentList.push(searchData);
+            } else {
+                resentList = resentList.filter(item => item.filter.place_name.toLowerCase() != searchData.filter.place_name.toLowerCase());
+                resentList.unshift(searchData);
+            }
+            localStorage.setItem('recentSearch', JSON.stringify(resentList));
         }
-        localStorage.setItem('recentSearch', JSON.stringify(resentList));
     }
 
     applySearch(item){
@@ -113,12 +151,24 @@ export default class MainController {
 
     checkPreviewed () {
         var previewData = JSON.parse(localStorage.getItem('previewed'));
-        if (previewData.state) {
+        if (previewData && previewData.state) {
             var resentList = JSON.parse(localStorage.getItem('recentSearch'));
             this.applySearch(resentList[0]);
             this.$timeout(()=>{
                 window.scrollTo(0, previewData.offset);
             },10);
         }
+    }
+
+    myLoc() {
+        navigator.geolocation.getCurrentPosition(position=> {
+            this.filter.centre_point = position.coords.latitude + ',' + position.coords.longitude;
+            this.searchLoc();
+        });
+    }
+
+    locSearch(loc) {
+        this.search = loc.place_name;
+        this.searchLoc(false, loc.place_name);
     }
 }
